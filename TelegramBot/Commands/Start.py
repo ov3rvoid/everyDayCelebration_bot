ef84d4004c225d2ee1e2bb11e48aa2e9cf13dedb
Sergei Zhuravlev <worthless@MacBook-Pro-Sergei.local>
2023-10-11 23:58:20 +0300

    random_smiles

--- TelegramBot/Commands/Start.py
+++ TelegramBot/Commands/Start.py
@@ -1,6 +1,14 @@
 from Config import dp, bot
 from aiogram import types
 from aiogram.dispatcher.filters import Command
+from random import randint
+
+
+smiles = ['🥳', '🥹', '😉', '💩', '🧐', '😎', '😶‍🌫️', '👻', '🤡', '🤯', '🎉', '🕊️']
+len_smiles = len(smiles)
+def random_index(len_smiles):
+    index = randint(0, len_smiles)
+    return index
 
 
 @dp.message_handler(commands= ['start'])
@@ -8,4 +16,12 @@ async def start(message: types.Message):
     await bot.send_message(
         message.from_user.id,
         "Меня создали два гения"
+    )
+
+@dp.message_handler(commands=['celebrate'])
+async def celebrate(message: types.Message):
+    msg = message.text
+    await bot.send_message(
+        message.from_user.id,
+        f"{smiles[random_index(len_smiles)]}инициализирую поиск праздника\n\n{smiles[random_index(len_smiles)]}ща дропну",
     )
