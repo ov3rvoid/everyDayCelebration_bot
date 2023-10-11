9f3250e432fdac642aa10d9c5a802192092d2293
Sergei Zhuravlev <gapordie2@gmail.com>
2023-10-12 00:14:46 +0300

    add_get_date

--- TelegramBot/Commands/Start.py
+++ TelegramBot/Commands/Start.py
@@ -2,10 +2,16 @@ from Config import dp, bot
 from aiogram import types
 from aiogram.dispatcher.filters import Command
 from random import randint
+from datetime import datetime 
 
+
+newdate = datetime.now()  
+newdate = newdate.strftime("%d.%m.%Y")
 
 smiles = ['🥳', '🥹', '😉', '💩', '🧐', '😎', '😶‍🌫️', '👻', '🤡', '🤯', '🎉', '🕊️']
-len_smiles = len(smiles)
+len_smiles = len(smiles) - 1
+
+
 def random_index(len_smiles):
     index = randint(0, len_smiles)
     return index
@@ -18,9 +24,8 @@ async def start(message: types.Message):
         "Меня создали два гения"
     )
 
-@dp.message_handler(commands=['celebrate'])
+@dp.message_handler(commands='celebrate')
 async def celebrate(message: types.Message):
-    msg = message.text
     await bot.send_message(
         message.from_user.id,
         f"{smiles[random_index(len_smiles)]}инициализирую поиск праздника\n\n{smiles[random_index(len_smiles)]}ща дропну",
