a7f926fe42a26def9cecf5eddd4bb615ad142768
Sergei Zhuravlev <gapordie2@gmail.com>
2023-10-12 01:09:50 +0300

    demoparser

--- TelegramBot/Parser/parsing.py
+++ TelegramBot/Parser/parsing.py
@@ -1,2 +1,13 @@
 from bs4 import BeautifulSoup
 import requests
+
+url = 'https://kakoysegodnyaprazdnik.ru/'
+page = requests.get(url)
+
+filteredCelebrates = []
+allCelebrates = []
+soup = BeautifulSoup(page.text, "html.parser")
+allCelebrates = soup.findAll('div', class_ = 'other')
+for celebrates in allCelebrates:
+    if celebrates.find('span', class_='super'):
+        print(celebrates.text)
