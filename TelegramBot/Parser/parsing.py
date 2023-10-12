e4049967c3ed152b41ed5210d060d29643c2ae1f
Глеб Трушков <gleb050305@gmail.com>
2023-10-12 13:12:25 +0300

    parser

--- TelegramBot/Parser/parsing.py
+++ TelegramBot/Parser/parsing.py
@@ -1,18 +1,60 @@
 from bs4 import BeautifulSoup
 import requests
+import string 
+from datetime import datetime
 
-url = 'https://kakoysegodnyaprazdnik.ru/'
+newdate = datetime.now()
+newdate = newdate.strftime("%d.%m.%Y")
+date_list = newdate.split('.')
+
+months = [
+    'yanvar',
+    'fevral',
+    'mart',
+    'aprel',
+    'may',
+    'iyun',
+    'iyul',
+    'avgust',
+    'sentyabr',
+    'oktyabr',
+    'noyabr',
+    'decabr'
+]
+
+month = months[int(date_list[1]) - 1]
+day = date_list[0]
+
+url = 'https://kakoysegodnyaprazdnik.ru/baza/' + month + '/' + day
 page = requests.get(url)
+print(page)
 page.encoding = 'utf-8'
 
 filteredCelebrates = []
 allCelebrates = []
-
+print(date_list)
 soup = BeautifulSoup(page.text, "html.parser")
 allCelebrates = soup.findAll('div', class_ = 'other')
 
+forbidden_symbols = string.ascii_letters + string.digits + '()'
+
 for celebrates in allCelebrates:
     if celebrates.find('span', class_='super'):
-        filteredCelebrates.append(celebrates.text)
+        celebrates_of_day = celebrates.text
+        final_text = ''
+        mb_its_abb = 0
+        for symb in celebrates_of_day:
+            if mb_its_abb == 1 and symb.isupper():
+                final_text = final_text[:-1]
+                break
+            else:
+                mb_its_abb = 0
+            if symb.isupper():
+                mb_its_abb = 1
+            if symb not in forbidden_symbols:
+                final_text += symb
+        final_text = final_text.replace('лет', '')
+        final_text = final_text.replace('года', '')
+        filteredCelebrates.append(final_text.strip())
 
 print(filteredCelebrates[:10])
