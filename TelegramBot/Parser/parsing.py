354d10d88555e859d7febd247f2db4f21fc50320
Глеб Трушков <gleb050305@gmail.com>
2023-10-13 12:00:21 +0300

    new_parser_wirh_selenium

--- TelegramBot/Parser/parsing.py
+++ TelegramBot/Parser/parsing.py
@@ -1,5 +1,3 @@
-# from bs4 import BeautifulSoup
-# import requests
 import string 
 from datetime import datetime
 from selenium import webdriver
@@ -8,7 +6,6 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import pandas as pd
 import time
 
 newdate = datetime.now()
@@ -33,7 +30,6 @@ months = [
 month = months[int(date_list[1]) - 1]
 day = date_list[0]
 
-<<<<<<< HEAD
 url = r'https://kakoysegodnyaprazdnik.ru/baza/' + month + '/' + day
 
 options = Options()
@@ -41,58 +37,34 @@ options.add_argument("--disable-infobars")
 browser = webdriver.Chrome(options=options)
 browser.get(url)
 
-elems  =  browser.find_elements(By.CLASS_NAME,  'listing_wr' )
-time.sleep(4)
+elems  =  browser.find_elements(By.CLASS_NAME,  'main' )
+allCelebrates = list()
+for el in elems:
+    allCelebrates.append(el.text)
+time.sleep(1)
 
-# page = requests.get(url)
-# print(url)
-# print(page)
-# print(page.text)
-# page.encoding = 'utf-8'
-=======
-url = f'https://kakoysegodnyaprazdnik.ru/'
-page = requests.get(url)
-print(page)
-page.encoding = 'utf-8'
->>>>>>> 0a57ad34f60d66fe802a9269b798f80d4a6661b5
-
-
-filteredCelebrates = []
-allCelebrates = []
-<<<<<<< HEAD
-# print(date_list)
-# soup = BeautifulSoup(page.text, "html.parser")
-# allCelebrates = soup.findAll('div', class_ = 'main')
-print(allCelebrates)
+filteredCelebrates = list()
 
 forbidden_symbols = string.ascii_letters + string.digits + '()'
 
-=======
+for celebrate in allCelebrates:
+    final_text = ''
+    mb_its_abb = 0
+    for symb in celebrate:
+        if mb_its_abb == 1 and symb.isupper():
+            final_text = final_text[:-1]
+            break
+        else:
+            mb_its_abb = 0
+        if symb.isupper():
+            mb_its_abb = 1
+        if symb not in forbidden_symbols:
+            final_text += symb
+    for _ in range(4): final_text = final_text.replace('  ', ' ')
+    filteredCelebrates.append(final_text.strip('• ' + string.punctuation))
 
-soup = BeautifulSoup(page.text, "html.parser")
-allCelebrates = soup.findAll('div', class_ = 'event_block')
-forbidden_symbols = string.ascii_letters + '()'
-# print(allCelebrates)
->>>>>>> 0a57ad34f60d66fe802a9269b798f80d4a6661b5
-for celebrates in allCelebrates:
-    if celebrates.find('span', class_='other'):
-        celebrates_of_day = celebrates.text
-        final_text = ''
-        mb_its_abb = 0
-        for symb in celebrates_of_day:
-            if mb_its_abb == 1 and symb.isupper():
-                final_text = final_text[:-1]
-                break
-            else:
-                mb_its_abb = 0
-            if symb.isupper():
-                mb_its_abb = 1
-            if symb not in forbidden_symbols:
-                final_text += symb
-        # final_text = final_text.replace('лет', '')
-        # final_text = final_text.replace('года', '')
-        filteredCelebrates.extend(final_text.split('•'))
-
-for i in range(len(filteredCelebrates) - 1):
+for i in range(len(filteredCelebrates)):
     if filteredCelebrates[i] == '':
         filteredCelebrates.pop(i)
+    print(filteredCelebrates[i])
+
