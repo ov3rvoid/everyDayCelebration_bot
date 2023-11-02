efcb37cc66815fc0d92e20517959fde2ca73721f
ov3rvoid <gapordie2@gmail.com>
2023-11-02 12:48:38 +0300

    release

--- TelegramBot/Parser/holidays_dropper.py
+++ TelegramBot/Parser/holidays_dropper.py
@@ -1,12 +1,13 @@
 import pandas as pd
-import parsing as prs
+from .parsing import date_list, formatting_all_celebrates
 
-path_to_data = 'TelegramBot/Data/AllHolidays.csv'
-date_list = prs.date_list
+
+path_to_data = 'Data/AllHolidays.csv'
+date_list = date_list
 
 
 data = pd.read_csv(path_to_data, sep=';')
 date_name = f'{date_list[0]}/{date_list[1]}'
 holidays = data[date_name]
 holidays = holidays[holidays.notnull()]
-output_celebrates = prs.formatting_all_celebrates(holidays)
+output_celebrates = formatting_all_celebrates(holidays)
